f5f052e06932534ece3451be7128e812130f4251
sxdtzhaoxinguo <sxdtzhaoxinguo@163.com>
2015-07-15 15:57:19 +0800

    解决了空指针异常，原因是dao的包名路径不对

---
+++ src/main/java/org/micaicms/ssmb/infrastructure/dao/imp/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * <p>Copyright ® 迷彩CMS平台版权所有。</p>
+ * 数据操作层实现
+ */
+package org.micaicms.ssmb.infrastructure.dao.imp;
---
+++ src/main/java/org/micaicms/ssmb/util/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * <p>Copyright ® 迷彩CMS平台版权所有。</p>
+ * 工具类层
+ *
+ */
+package org.micaicms.ssmb.util;
